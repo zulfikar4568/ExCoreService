48e30eb33e7881bdc59d56fa2db1220190c01864
M. Zulfikar Isnaen <isnaen70@gmail.com>
2021-12-20 00:06:39 +0700

    Add project files.

---
+++ ExCoreService/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace ExCoreService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<StreamFile>(s =>
+                {
+                    s.ConstructUsing(streamfile => new StreamFile());
+                    s.WhenStarted(streamfile => streamfile.Start());
+                    s.WhenStopped(streamfile => streamfile.Stop());
+                });
+                x.RunAsLocalSystem();
+                x.SetServiceName("ExCoreService");
+                x.SetDisplayName("ExCore Service");
+                x.SetDescription("This is service for MES Opcenter Execution Core");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
---
+++ ExCoreService/StreamFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ExCoreService
+{
+    public class StreamFile
+    {
+        private readonly Timer _timer;
+
+        public StreamFile()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\Temp\TestFile.txt", lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
